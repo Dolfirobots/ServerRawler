f27c6fc133d5a84511d446dbcc7a438968cf3b62
Cyberdolfi <cyberdolfi@gmail.com>
2026-02-17 16:20:42 +0100

    feat(database): rework the entire database saving logic and add player insert

--- src/database/server.rs
+++ src/database/server.rs
@@ -1,70 +1,107 @@
 use sqlx::{Postgres, Transaction};
 use crate::database::{parse_players, pool, Player, PlayerHistory, ServerHistory, ServerInfo};
 
-pub async fn insert_servers(servers: Vec<(ServerInfo, ServerHistory)>) -> Result<(), sqlx::Error> {
+pub async fn insert_servers(results: Vec<(ServerInfo, ServerHistory)>) -> Result<(), sqlx::Error> {
     let pool = pool::get_pool();
 
-    let mut tx: Transaction<'_, Postgres> = pool.begin().await?;
+    for chunk in results.chunks(100) {
+        let mut tx = pool.begin().await?;
 
-    for (server, history) in servers {
-        let row: (i32,) = sqlx::query_as(
+        for (server, server_history) in chunk {
+            let (server_id, ): (i32,) = sqlx::query_as(
+                r#"
+                INSERT INTO servers (server_ip, server_port, last_seen, discovered, bedrock, country)
+                VALUES ($1, $2, $3, $4, $5, $6)
+                ON CONFLICT (server_ip, server_port)
+                DO UPDATE SET last_seen = EXCLUDED.last_seen
+                RETURNING server_id
+                "#
+            )
+                .bind(&server.server_ip)
+                .bind(server.server_port as i32)
+                .bind(server.last_seen)
+                .bind(server.discovered)
+                .bind(server.bedrock)
+                .bind(&server.country)
+                .fetch_one(&mut *tx)
+                .await?;
+
+            sqlx::query(
+                r#"
+                INSERT INTO server_history (
+                    server_id, seen, description, plain_description, icon,
+                    player_online, player_max, player_sample,
+                    version_name, version_protocol, enforces_secure_chat,
+                    is_modded_server, mods, mod_loader,
+                    players, plugins, software,
+                    kick_message, cracked, whitelist, latency
+                )
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)
+                "#
+            )
+                .bind(server_id)
+                .bind(server_history.seen)
+                .bind(&server_history.description)
+                .bind(&server_history.plain_description)
+                .bind(&server_history.icon)
+                .bind(server_history.player_online)
+                .bind(server_history.player_max)
+                .bind(serde_json::to_value(&server_history.player_sample).unwrap_or(serde_json::Value::Null))
+                .bind(&server_history.version_name)
+                .bind(server_history.version_protocol)
+                .bind(server_history.enforces_secure_chat)
+                .bind(server_history.is_modded_server)
+                .bind(serde_json::to_value(&server_history.mods).unwrap_or(serde_json::Value::Null))
+                .bind(serde_json::to_value(&server_history.mod_loader).unwrap_or(serde_json::Value::Null))
+                .bind(serde_json::to_value(&server_history.players).unwrap_or(serde_json::Value::Null))
+                .bind(serde_json::to_value(&server_history.plugins).unwrap_or(serde_json::Value::Null))
+                .bind(serde_json::to_value(&server_history.software).unwrap_or(serde_json::Value::Null))
+                .bind(&server_history.kick_message)
+                .bind(server_history.cracked)
+                .bind(server_history.whitelist)
+                .bind(server_history.latency)
+                .execute(&mut *tx)
+                .await?;
+
+            let players = parse_players(server_id, server_history);
+            insert_players(players, &mut tx).await?;
+        }
+        tx.commit().await?;
+    }
+    Ok(())
+}
+
+pub async fn insert_players(player_data: Vec<(Player, PlayerHistory)>, tx: &mut Transaction<'_, Postgres>) -> Result<(), sqlx::Error> {
+    for (player, history) in player_data {
+        sqlx::query(
             r#"
-            INSERT INTO servers (server_ip, server_port, last_seen, discovered, bedrock, country)
-            VALUES ($1, $2, $3, $4, $5, $6)
-            ON CONFLICT (server_ip, server_port)
-            DO UPDATE SET last_seen = EXCLUDED.last_seen
-            RETURNING server_id
+            INSERT INTO players (uuid, username, discovered, last_seen)
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT (uuid)
+            DO UPDATE SET
+                last_seen = EXCLUDED.last_seen,
+                username = EXCLUDED.username
             "#
         )
-            .bind(&server.server_ip)
-            .bind(server.server_port as i32)
-            .bind(server.last_seen)
-            .bind(server.discovered)
-            .bind(server.bedrock)
-            .bind(&server.country)
-            .fetch_one(&mut *tx)
+            .bind(&player.uuid)
+            .bind(&player.username)
+            .bind(player.discovered)
+            .bind(player.last_seen)
+            .execute(&mut **tx)
             .await?;
-
-        let server_id = row.0;
 
         sqlx::query(
             r#"
-            INSERT INTO server_history (
-                server_id, seen, description, plain_description, icon,
-                player_online, player_max, player_sample,
-                version_name, version_protocol, enforces_secure_chat,
-                is_modded_server, mods, mod_loader,
-                players, plugins, software,
-                kick_message, cracked, whitelist, latency
-            )
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)
+            INSERT INTO player_history (uuid, username, server_id, seen)
+            VALUES ($1, $2, $3, $4)
             "#
         )
-            .bind(server_id)
+            .bind(&history.uuid)
+            .bind(&history.username)
+            .bind(history.server_id)
             .bind(history.seen)
-            .bind(&history.description)
-            .bind(&history.plain_description)
-            .bind(&history.icon)
-            .bind(history.player_online)
-            .bind(history.player_max)
-            .bind(serde_json::to_value(&history.player_sample).ok())
-            .bind(&history.version_name)
-            .bind(history.version_protocol)
-            .bind(history.enforces_secure_chat)
-            .bind(history.is_modded_server)
-            .bind(serde_json::to_value(&history.mods).ok())
-            .bind(serde_json::to_value(&history.mod_loader).ok())
-            .bind(serde_json::to_value(&history.players).ok())
-            .bind(serde_json::to_value(&history.plugins).ok())
-            .bind(serde_json::to_value(&history.software).ok())
-            .bind(&history.kick_message)
-            .bind(history.cracked)
-            .bind(history.whitelist)
-            .bind(history.latency)
-            .execute(&mut *tx)
+            .execute(&mut **tx)
             .await?;
     }
-
-    tx.commit().await?;
     Ok(())
 }
