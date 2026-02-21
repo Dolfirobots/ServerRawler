301624d0e77309c2b60c3d8090172b469b354006
Cyberdolfi <cyberdolfi@gmail.com>
2026-02-21 11:52:38 +0100

    feat(network): add DNS resolver

--- src/scanning/mod.rs
+++ src/scanning/mod.rs
@@ -3,3 +3,15 @@ pub mod file_scanner;
 pub mod utils;
 pub mod crawler;
 pub mod rescanner;
+
+pub async fn resolve_address(hostname: &str, port: u16) -> Option<String> {
+    if let Ok(_) = hostname.parse::<std::net::IpAddr>() {
+        return Some(hostname.to_string());
+    }
+    
+    tokio::net::lookup_host(format!("{}:{}", hostname, port)).await.ok()?
+        .next()?
+        .ip()
+        .to_string()
+        .into()
+}
