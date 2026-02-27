721fe7a6a8957024927f8ed6b9f02fa63f29cd8e
Cyberdolfi <cyberdolfi@gmail.com>
2026-02-27 17:35:02 +0100

    feat(dns resolver): now returning Ip4Addr

--- src/scanning/mod.rs
+++ src/scanning/mod.rs
@@ -1,17 +1,28 @@
+use std::net::Ipv4Addr;
+use tokio::net::lookup_host;
+use crate::logger;
+
 pub mod scanner;
 pub mod file_scanner;
 pub mod utils;
 pub mod crawler;
 pub mod rescanner;
 
-pub async fn resolve_address(hostname: &str, port: u16) -> Option<String> {
-    if let Ok(_) = hostname.parse::<std::net::IpAddr>() {
-        return Some(hostname.to_string());
+// TODO: It can't find play.hypixel.net
+pub async fn resolve_address(hostname: &str, port: u16) -> Option<Ipv4Addr> {
+    if let Ok(ip) = hostname.parse::<Ipv4Addr>() {
+        return Some(ip);
     }
-    
-    tokio::net::lookup_host(format!("{}:{}", hostname, port)).await.ok()?
-        .next()?
-        .ip()
-        .to_string()
-        .into()
+
+    let addresses = lookup_host(format!("{}:{}", hostname, port)).await.ok()?;
+    // Currently ServerRawler's code is dependent on Ipv4Addr because it saves so much RAM than other objs
+    // This will be maybe change in the future, you can help!
+    for addr in addresses {
+        logger::debug(addr.to_string()).prefix("DNS Lookup").send().await;
+
+        if let std::net::IpAddr::V4(ipv4) = addr.ip() {
+            return Some(ipv4);
+        }
+    }
+    None
 }
