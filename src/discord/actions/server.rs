29dd080025b2ebf02ca76fbf31b0b3f2e12d89e1
Cyberdolfi <cyberdolfi@gmail.com>
2026-02-28 14:00:39 +0100

    feat(actions): extract action handler in a new method

--- src/discord/actions/server.rs
+++ src/discord/actions/server.rs
@@ -6,14 +6,15 @@ use base64::prelude::BASE64_STANDARD;
 use chrono::{DateTime, Utc};
 use futures::StreamExt;
 use poise::ReplyHandle;
-use serenity::all::{ButtonStyle, ComponentInteractionCollector, CreateActionRow, CreateAttachment, CreateButton, CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage, EditAttachments, EditInteractionResponse, EditMessage};
+use serenity::all::{ButtonStyle, ComponentInteraction, ComponentInteractionCollector, CreateActionRow, CreateAttachment, CreateButton, CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage, EditAttachments, EditInteractionResponse, EditMessage};
 use crate::database::{ServerHistory, ServerInfo};
 use crate::discord::{create_base_embed, create_error_embed, create_loading_embed, Context};
 use crate::logger;
 use crate::minecraft::{join, ping, query};
 
 // TODO: Add error msgs
-fn build_manage_server_action_row(disabled: bool, history: &ServerHistory) -> Vec<CreateActionRow> {
+
+pub fn build_manage_server_action_row(disabled: bool, history: &ServerHistory) -> Vec<CreateActionRow> {
     let show_plugins = history.plugins.as_ref().map_or(0, |p| p.len()) > 0;
     let show_mods = history.mods.as_ref().map_or(0, |m| m.len()) > 0;
     let show_player_sample = history.player_sample.as_ref().map_or(0, |p| p.len()) > 0;
@@ -61,7 +62,7 @@ fn build_manage_server_action_row(disabled: bool, history: &ServerHistory) -> Ve
     ]
 }
 
-fn build_server_embed(start_time: DateTime<Utc>, info: &ServerInfo, history: &ServerHistory) -> CreateEmbed {
+pub fn build_server_embed(start_time: DateTime<Utc>, info: &ServerInfo, history: &ServerHistory) -> CreateEmbed {
     let mut embed = create_base_embed(Some(start_time))
         .title("Server Overview")
         .color(0x2ecc71);
@@ -143,7 +144,6 @@ pub async fn create_one_server_action(
     server_info: ServerInfo,
     server_history: ServerHistory
 ) -> Result<(), serenity::Error> {
-
     let mut response = EditMessage::new().embed(build_server_embed(start_time, &server_info, &server_history));
 
     if let Some(base64_icon) = &server_history.icon {
@@ -168,65 +168,7 @@ pub async fn create_one_server_action(
         .stream();
 
     while let Some(mci) = collector.next().await {
-        match mci.data.custom_id.as_str() {
-            "relookup" => {
-                let start_time = Utc::now();
-                mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::UpdateMessage(
-                    CreateInteractionResponseMessage::new()
-                        .embed(create_loading_embed("pinging the server (May take 6 sec)"))
-                        .components(vec![])
-                )).await?;
-
-                if let Ok(ip) = Ipv4Addr::from_str(server_info.server_ip.as_str()) {
-                    if let Ok(ping_result) = ping::execute_ping(ip, server_info.server_port, 0, Duration::from_secs(3)).await {
-                        let query = query::execute_query(ip, server_info.server_port, Duration::from_secs(3), true).await.ok();
-                        let join = join::execute_join_check(ip, server_info.server_port, Duration::from_secs(3), "ServerRawler", ping_result.protocol_version.or(server_history.version_protocol).unwrap_or(767)).await.ok();
-                        let (mut new_info, new_history) = crate::database::parse_server(ip, server_info.server_port, ping_result, query, None);
-
-                        if crate::database::server::insert_servers(
-                            &vec![
-                                (new_info.clone(), new_history.clone())
-                            ]
-                        ).await.is_ok() {
-                            new_info.discovered = server_info.discovered;
-                            new_info.server_id = server_info.server_id;
-
-                            let mut inter_response = EditInteractionResponse::new()
-                                .embed(build_server_embed(start_time, &new_info, &new_history))
-                                .components(build_manage_server_action_row(false, &new_history));
-
-                            if let Some(base64_icon) = &new_history.icon {
-                                if let Ok(decoded_bytes) = BASE64_STANDARD.decode(base64_icon.trim()) {
-                                    let attachment = CreateAttachment::bytes(decoded_bytes, "server_icon.png");
-                                    inter_response = inter_response.attachments(EditAttachments::new().add(attachment.clone()));
-                                    response = response.attachments(EditAttachments::new().add(attachment))
-                                }
-                            }
-                            response = response.embed(build_server_embed(start_time, &server_info, &server_history));
-
-                            mci.edit_response(&ctx.serenity_context().http, inter_response).await?;
-                            continue;
-                        }
-                    }
-                }
-
-                mci.edit_response(&ctx.serenity_context().http, EditInteractionResponse::new()
-                    .embed(create_error_embed("Failed to ping server", None))
-                    .components(build_manage_server_action_row(false, &server_history))
-                ).await?;
-                return Ok(())
-            },
-            "show_history" => {
-                mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::UpdateMessage(
-                    CreateInteractionResponseMessage::new()
-                        .embed(create_loading_embed("coding this feature"))
-                        .components(vec![])
-                )).await?;
-                // TODO: Give user the complete history of the server
-                //  with paginator
-            },
-            _ => {}
-        }
+        handle_server_action(ctx, &mci, &server_info, &server_history).await?;
     }
 
     message.edit(
@@ -234,5 +176,112 @@ pub async fn create_one_server_action(
         response.components(build_manage_server_action_row(true, &server_history))
     ).await?;
 
+    Ok(())
+}
+
+pub async fn handle_server_action(
+    ctx: Context<'_>,
+    mci: &ComponentInteraction,
+    server_info: &ServerInfo,
+    server_history: &ServerHistory,
+) -> Result<(), serenity::Error> {
+    match mci.data.custom_id.as_str() {
+        "relookup" => {
+            let start_time = Utc::now();
+            mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::UpdateMessage(
+                CreateInteractionResponseMessage::new()
+                    .embed(create_loading_embed("Pinging the server (may take 6s)..."))
+                    .components(vec![])
+            )).await?;
+
+            if let Ok(ip) = Ipv4Addr::from_str(server_info.server_ip.as_str()) {
+                if let Ok(ping_result) = ping::execute_ping(ip, server_info.server_port, 0, Duration::from_secs(3)).await {
+                    let query = query::execute_query(ip, server_info.server_port, Duration::from_secs(3), true).await.ok();
+                    let protocol = ping_result.protocol_version.or(server_history.version_protocol).unwrap_or(767);
+                    let join = join::execute_join_check(ip, server_info.server_port, Duration::from_secs(3), "ServerRawler", protocol).await.ok();
+
+                    let (mut new_info, new_history) = crate::database::parse_server(ip, server_info.server_port, ping_result, query, join);
+
+                    if crate::database::server::insert_servers(&vec![(new_info.clone(), new_history.clone())]).await.is_ok() {
+                        new_info.discovered = server_info.discovered;
+                        new_info.server_id = server_info.server_id;
+
+                        let mut edit_resp = EditInteractionResponse::new()
+                            .embed(build_server_embed(start_time, &new_info, &new_history))
+                            .components(build_manage_server_action_row(false, &new_history));
+
+                        if let Some(base64_icon) = &new_history.icon {
+                            if let Ok(decoded_bytes) = BASE64_STANDARD.decode(base64_icon.trim()) {
+                                edit_resp = edit_resp.attachments(EditAttachments::new().add(CreateAttachment::bytes(decoded_bytes, "server_icon.png")));
+                            }
+                        }
+                        mci.edit_response(&ctx.serenity_context().http, edit_resp).await?;
+                        return Ok(());
+                    }
+                }
+            }
+            mci.edit_response(&ctx.serenity_context().http, EditInteractionResponse::new()
+                .embed(create_error_embed("Failed to ping server", None))
+                .components(build_manage_server_action_row(false, server_history))
+            ).await?;
+        },
+        "show_plugins" => {
+            let plugins = server_history.plugins.as_ref().map(|p| {
+                p.iter().map(|pl| format!("`{}` ({})", pl.name, pl.version.clone())).collect::<Vec<_>>().join("\n")
+            }).unwrap_or_else(|| "No plugins found.".to_string());
+
+            mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new()
+                    .embed(create_base_embed(None).title("Plugins").description(plugins))
+                    .ephemeral(true)
+            )).await?;
+        },
+        "show_mods" => {
+            let mods = server_history.mods.as_ref().map(|m| {
+                m.iter().map(|mo| format!("`{}`", mo.name)).collect::<Vec<_>>().join(", ")
+            }).unwrap_or_else(|| "No mods found.".to_string());
+
+            mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new()
+                    .embed(create_base_embed(None).title("Mods").description(mods))
+                    .ephemeral(true)
+            )).await?;
+        },
+        "show_player_sample" | "show_players" => {
+            let players = if mci.data.custom_id == "show_players" {
+                server_history.players.as_ref().map(|p| {
+                    p.iter()
+                        .filter_map(|pl| pl.name.as_deref())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                })
+            } else {
+                server_history.player_sample.as_ref().map(|p| {
+                    p.iter()
+                        .filter_map(|pl| pl.name.as_deref())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                })
+            }.unwrap_or_else(|| "No players found.".to_string());
+
+            let final_players = if players.is_empty() { "No players found.".to_string() } else { players };
+
+            mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new()
+                    .embed(create_base_embed(None).title("Player List").description(final_players))
+                    .ephemeral(true)
+            )).await?;
+        },
+        "show_history" => {
+            mci.create_response(&ctx.serenity_context().http, CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new()
+                    .embed(create_loading_embed("coding this feature"))
+                    .components(vec![])
+            )).await?;
+            // TODO: Give user the complete history of the server
+            //  with paginator
+        },
+        _ => {}
+    }
     Ok(())
 }
